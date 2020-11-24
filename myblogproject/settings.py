933bcf909ea3f0330277a0a169b228bc17b5a924
Prabesh Nepal <prabesh.vacker360@gmail.com>
2020-11-24 21:51:38 +0545

    Update settings.py,email

--- myblogproject/settings.py
+++ myblogproject/settings.py
@@ -168,9 +168,7 @@ LOGOUT_REDIRECT_URL = '/login/'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
-# EMAIL_HOST_USER = env('EMAIL_HOST_USER')
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
-# EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 EMAIL_PORT = 587
 
