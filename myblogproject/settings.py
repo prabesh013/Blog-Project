939802ccc48119d3902f29cb4d880fbf341c9fc3
Prabesh Nepal <prabesh.vacker360@gmail.com>
2020-11-24 20:48:25 +0545

    Update DEBUG

--- myblogproject/settings.py
+++ myblogproject/settings.py
@@ -38,7 +38,7 @@ TEMPLATE_DIR_TWO = BASE_DIR / "users/templates/users"
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1','localhost','prabesh-django-blog.herokuapp.com']
 
